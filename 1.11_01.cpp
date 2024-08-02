c73f6d77f1dc09e2d4f1bb4611fb723389e4b29f
sokol <sokol@ASUS>
2024-08-02 21:20:19 +0500

    Добавьте файлы проекта.

---
+++ 1.11_01.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<windows.h>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::string name;
+	std::string sername;
+	std::string str3;
+
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	
+	std::cout << "Введите фамилию: ";
+	std::cin >> sername;
+
+	str3 = "Здравствуйте, " + name + " " + sername + "!";
+	
+	std::cout << str3;
+
+	std::cout << "\n\n\n";
+
+	return 0;
+}
